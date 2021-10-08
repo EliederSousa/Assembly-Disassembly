dc10eb8ea6832e7422ad6733e64854f057d8fc4d
EliederSousa <eliedersousa@gmail.com>
2021-10-08 12:51:34 -0300

    Update LAB 6 - Switch.asm

--- MIPS/LAB 6 - Switch.asm
+++ MIPS/LAB 6 - Switch.asm
@@ -1,11 +1,8 @@
-# Nome: Elieder Damasceno Sousa
-# TIA: 32093659		Turma: 03G11
-
 .data
 	jTable: .word L1,L2,L3,L4,L5
 
 .text
-	la $t4, jTable 		# Carrega o endereço de memória da tabela
+	la $t4, jTable 		# Carrega o endereÃ§o de memÃ³ria da tabela
 	li $s5, 1 		# w
 	li $s0, 4		# a
 	li $s1, 0		# b = 0
@@ -15,14 +12,14 @@
 	slti $t3, $s5, 6	# $t3 = (w < 6)
 	beq $t3, $zero, SAIR	# if ($t3 == 0) jmp SAIR
 	
-	# w = w - 1 (fiz assim por que o switch avalia casos entre 1 e 5, não entre 0 e 4)
-	# portanto, o sll nunca carregaria o primeiro endereço da jTable, e daria erro ao tentar carregar
-	# quando w = 5 (porque 5 * 4 = 20, e o último endereço da jTable é 16($t1) )
+	# w = w - 1 (fiz assim por que o switch avalia casos entre 1 e 5, nÃ£o entre 0 e 4)
+	# portanto, o sll nunca carregaria o primeiro endereÃ§o da jTable, e daria erro ao tentar carregar
+	# quando w = 5 (porque 5 * 4 = 20, e o Ãºltimo endereÃ§o da jTable Ã© 16($t1) )
 	subi $s5, $s5, 1
 	
 	sll $t1, $s5, 2		# $t1 = w * 4 
-	add $t1, $t1, $t4	# $t1 = &$t4 + $t1 (desloca o endereço $t4, $t1 bytes, e guarda em $t1)
-	lw $t0, 0($t1)		# $t0 = *($t1) ($t0 agora é o valor que está dentro do endereço $t1)
+	add $t1, $t1, $t4	# $t1 = &$t4 + $t1 (desloca o endereÃ§o $t4, $t1 bytes, e guarda em $t1)
+	lw $t0, 0($t1)		# $t0 = *($t1) ($t0 agora Ã© o valor que estÃ¡ dentro do endereÃ§o $t1)
 	
 	jr $t0			# Segue para o Label indicado em $t0
 L1:
